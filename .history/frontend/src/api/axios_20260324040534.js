94a4ca0e17e7b6fc67041773d965215ce051e190
Mahmud Hasan Shanto <sshanto12344321@gmail.com>
2026-03-26 16:35:24 +0600

    connect frontend to Render backend

--- .history/frontend/src/api/axios_20260324040534.js
+++ .history/frontend/src/api/axios_20260324040534.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend-name.onrender.com", // 🔥 CHANGE THIS
+  baseURL: "http://localhost:8080",
 });
 
 API.interceptors.request.use(
